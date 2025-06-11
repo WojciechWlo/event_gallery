e6f5c141957940dc2d2cd0df07a3d5726058a14e
Wojciech Włódarczak <vojciechandre@gmail.com>
2025-06-11 23:03:32 +0200

    fixed problem with uploading video and saving it to pgsql

--- initdb.sql
+++ initdb.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    hashed_password TEXT
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    hashed_password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS uploads (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY,
     nickname TEXT,
     datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS media (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    filename TEXT,
+    id INTEGER PRIMARY KEY,
+    filename TEXT NOT NULL,
     mediatype TEXT NOT NULL CHECK(mediatype IN ('image', 'video')),
     upload_id INTEGER,
     FOREIGN KEY (upload_id) REFERENCES uploads(id) ON DELETE CASCADE
