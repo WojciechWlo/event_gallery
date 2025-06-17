06c2745bcc83a72463211c78bf585147ab82eae7
Wojciech Włódarczak <vojciechandre@gmail.com>
2025-06-17 21:38:38 +0200

    creating two .sql files for dev and prod

--- initdb.prod.sql
+++ initdb.prod.sql
@@ -1,4 +1,22 @@
--- create_tech_user.sql
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    hashed_password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS uploads (
+    id SERIAL PRIMARY KEY,
+    nickname TEXT,
+    datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS media (
+    id SERIAL PRIMARY KEY,
+    filename TEXT NOT NULL,
+    mediatype TEXT NOT NULL CHECK(mediatype IN ('image', 'video')),
+    upload_id INTEGER,
+    FOREIGN KEY (upload_id) REFERENCES uploads(id) ON DELETE CASCADE
+);
 
 DO $$
 DECLARE
--- load_sqlite3.sh
+++ load_sqlite3.sh
@@ -3,8 +3,8 @@
 DB_FILE="/app/db.sqlite3"
 
 if [ ! -f "$DB_FILE" ]; then
-  echo "Creating SQLite DB from initdb.sql..."
-  sqlite3 "$DB_FILE" < /initdb.sql
+  echo "Creating SQLite DB from initdb.dev.sql..."
+  sqlite3 "$DB_FILE" < /initdb.dev.sql
 else
   echo "DB already exists, skipping init."
 fi
