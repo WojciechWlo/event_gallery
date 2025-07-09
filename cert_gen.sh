be0b1410d4589b491afdd16f38e8d0390cab2108
Wojciech Włódarczak <vojciechandre@gmail.com>
2025-07-09 19:36:40 +0200

    translating cert_gen.sh comments

--- cert_gen.sh
+++ cert_gen.sh
@@ -4,10 +4,10 @@ CERT_DIR=/certs
 KEY_FILE=$CERT_DIR/server.key
 CERT_FILE=$CERT_DIR/server.cert
 
-# Twórz folder certs jeśli nie ma
+# Create folder certs if does not exist
 mkdir -p $CERT_DIR
 
-# Generuj certyfikaty tylko jeśli ich brak
+# Generate certificates only if they do not exist
 if [ ! -f "$KEY_FILE" ] || [ ! -f "$CERT_FILE" ]; then
   echo "Generating SSL certificates..."
   openssl req -x509 -newkey rsa:4096 -nodes \
@@ -18,5 +18,5 @@ else
   echo "SSL certificates already exist."
 fi
 
-# Uruchom główny proces (komendę przekazaną do entrypoint)
+# Run main process
 exec "$@"
